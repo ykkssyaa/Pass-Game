75c336613c8999f9225d812abf5752b7f6c43ae5
ykkssyaa <64478650+ykkssyaa@users.noreply.github.com>
2022-08-23 01:41:35 +0300

    newLostTryLogic

---
+++ Assets/Scripts/LastTryConstructor.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LastTryConstructor : MonoBehaviour
+{
+    [SerializeField] private Image[] Indicators;
+    [SerializeField] private TextMeshProUGUI[] Nums;
+
+    private GameManager gameManager;
+
+    public Material MatchIndicatorMaterial;
+    public Material MatchWithPlaceIndicatorMaterial;
+    public Material InvisibleMaterial;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        InvisibleMaterial = gameManager.InvisibleMaterial;
+        MatchIndicatorMaterial = gameManager.MatchIndicatorMaterial;
+        MatchWithPlaceIndicatorMaterial = gameManager.MatchWithPlaceIndicatorMaterial;
+    }
+
+    public void SetNumbers(string InputNumbers, int NumCount)
+    {
+        for(int i = 0; i < NumCount; i++)
+        {
+            Nums[i].text = InputNumbers[i].ToString();
+        }
+    }
+
+    private void MakeIndicatorsInvisible()
+    {
+        foreach (var indicator in Indicators)
+        {
+            indicator.material = InvisibleMaterial;
+        }
+    }
+
+    public void SetIndicators(int equalNumbersWithSamePlace, int equalNumbers, int IndicatorsCount)
+    {
+        int countFirst = equalNumbersWithSamePlace;
+        int countSecond = equalNumbers;
+
+        MakeIndicatorsInvisible();
+
+        for (int i = 0; i < IndicatorsCount; i++)
+        {
+            if (countFirst > 0)
+            {
+                Indicators[i].material = MatchWithPlaceIndicatorMaterial;
+                countFirst--;
+            }
+            else if (countSecond > 0)
+            {
+                Indicators[i].material = MatchIndicatorMaterial;
+                countSecond--;
+            }
+        }
+    }
+}
